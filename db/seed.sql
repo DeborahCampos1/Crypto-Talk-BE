0ea05bb0288eb2fe2c73e31c3bbb191bd261907b
Deborah Campos <631care@gmail.com>
2022-05-24 20:02:51 -0400

    updated seed to slow headline readings

--- db/seed.sql
+++ db/seed.sql
@@ -2,43 +2,40 @@
 
 INSERT INTO resources(name, type, category, url, is_favorite) VALUES
 (
-    'Ethereum News', 'article', 'education', 
+    'Ethereum News...', 'article', 'education', 
     'https://news.bitcoin.com/bitcoin-ethereum-technical-analysis-eth-back-below-2000-as-btc-down-6-to-start-weekend/', 
     true
 ),
 (
-    'Bitcoin News"', 'article', 'news', 
+    'Bitcoin News...', 'article', 'news', 
     'https://news.bitcoin.com/bitcoin-ethereum-technical-analysis-eth-btc-surge-over-10-as-big-2-lead-crypto-rebound/', 
     true
 ),
 (
-    'Crypto Wallet', 'article', 'education', 
+    'Crypto Wallet...', 'article', 'education', 
     'https://news.bitcoin.com/rich-dad-poor-dads-robert-kiyosaki-plans-to-buy-bitcoin-when-the-bottom-is-in-17k/', 
     false
 ),
 (
-    'Bitcoin, Ethereum Technical Analysis', 'article', 'education', 
+    'Bitcoin, Ethereum Technical Analysis...', 'article', 'education', 
     'https://bitcoinmagazine.com/culture/what-makes-bitcoin-different-from-crypto', 
     true
 ),
 (
-    'Pursuit Announces Crypto for Fellows', 'article', 'news', 
+    'Pursuit Announces Crypto for Fellows...', 'article', 'news', 
     'https://news.bitcoin.com/coinbase-now-allows-cardano-staking-services-firm-plans-to-continue-to-scale-staking-portfolio/', 
     false
 ),
-('Crypto for Retirement', 'article', 'education', 
+('Crypto for Retirement...', 'article', 'education', 
     'https://news.bitcoin.com/tesla-ceo-elon-musk-reaffirms-dogecoin-has-potential-as-a-currency-as-twitter-deal-is-put-on-hold/', 
     false),
-('Teachers Donating Crypto', 'article', 'news', 
+('Teachers Donating Crypto...', 'article', 'news', 
     'https://news.bitcoin.com/cardano-to-reach-58-by-2030-according-to-finders-poll/', 
     false),
-('Pursuit Now Accepting Crypto', 'article', 'education',
+('Ethereum bounces back up...', 'article', 'education',
     'https://news.bitcoin.com/bitcoin-ethereum-technical-analysis-eth-bounces-back-up-2-while-btc-climbs-above-30000/', 
     true),
-('Bitcoin Market Crash', 'article', 'news', 
-    'https://news.bitcoin.com/top-diamond-producer-de-beers-deploys-blockchain-based-platform-at-scale/', 
-    true),
-('Crypto Platform Hack Rocks Blockchain Community', 'article', 'education', 
+('Crypto Platform Hack Rocks Blockchain Community...', 'article', 'education', 
     'https://www.bloomberg.com/news/features/2022-05-19/crypto-platform-hack-rocks-blockchain-community', 
     true),
 ('Crypto-loving S.Koreans bet on Luna rising from ashes, worrying regulator', 'article', 'news', 
