a0c13ccc356aade9f9b01362591acc0f15ea1087
Deborah Campos <631care@gmail.com>
2022-05-29 20:04:10 -0400

    test database updated with articles , videos and for filtered

--- db/seed.sql
+++ db/seed.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO resources(name, type, category, url, is_favorite) VALUES
 (
-    'Dodgecoin Jumps On Elon Musl Space X Tweet', 'article', 'news', 
+    'Dodgecoin Jumps On Elon Musk Space X Tweet', 'article', 'news', 
     'https://www.coindesk.com/business/2022/05/27/dogecoin-jumps-on-elon-musk-space-x-tweet/', 
     true
 ),
@@ -26,19 +26,83 @@ INSERT INTO resources(name, type, category, url, is_favorite) VALUES
     'https://cointelegraph.com/magazine/2022/01/25/security-experts-reveal-how-keep-crypto-safe', 
     true
 ),
-(   'Inflationary & Deflationary Cryptocurrencies', 'article', 'news', 
+(   
+    'Inflationary & Deflationary Cryptocurrencies', 'article', 'news', 
     'https://www.coindesk.com/learn/inflationary-and-deflationary-cryptocurrencies-whats-the-difference/', 
     true
 ),
-(   'Stripe Partners With Opennnode To Allow Instant Bitcoin Conversions for Businesses', 'article', 'news', 
+(   
+    'Stripe Partners With Opennnode To Allow Instant Bitcoin Conversions for Businesses', 'article', 'news', 
     'https://www.coindesk.com/business/2022/05/24/stripe-partners-with-opennode-to-allow-instant-fiat-to-bitcoin-conversions-for-businesses/', 
     true
 ),
-(   '6 Crypto Myths Debunked', 'article', 'news',
+(   
+    '6 Crypto Myths Debunked', 'article', 'news',
     'https://www.gobankingrates.com/investing/crypto/crypto-myths-debunked/', 
     true
 ),
-(   'How To Use Crypto For Buy Now Pay Later Transactions', 'article', 'education', 
+(   
+    'How To Use Crypto For Buy Now Pay Later Transactions', 'article', 'news', 
     'https://www.gobankingrates.com/investing/crypto/how-use-crypto-buy-now-pay-later-transactions/', 
+    true
+),
+(   
+    'What is Bitcoin?', 'article', 'education-article', 
+    'https://www.bitcoin.com/get-started/what-is-bitcoin/', 
+    true
+),
+(   
+    'How to Buy Bitcoin', 'article', 'education-article', 
+    'https://www.bitcoin.com/get-started/how-to-buy-bitcoin/', 
+    true
+),
+(   
+    'How to Sell Bitcoin', 'article', 'education-article', 
+    'https://www.bitcoin.com/get-started/how-to-sell-bitcoin/', 
+    true
+),
+(   
+    'What is a Bitcoin Wallet?', 'article', 'education-article', 
+    'https://www.bitcoin.com/get-started/what-is-a-bitcoin-wallet/', 
+    true
+),
+(   
+    'What is Ethereum?', 'article', 'education-article', 
+    'https://www.bitcoin.com/get-started/what-is-ethereum/', 
+    true
+),
+(   
+    'What is Ethereum Used For?', 'article', 'education-article', 
+    'https://www.bitcoin.com/get-started/what-is-eth-used-for/', 
+    true
+),
+(   
+    'What is Bitcoin? Bitcoin Explained Simply', 'video', 'education', 
+    'https://www.youtube.com/embed/41JCpzvnn_0', 
+    true
+),
+(   
+    'Bitcoin Cryptocurrency For Beginners 2022', 'video', 'education', 
+    'https://www.youtube.com/embed/WO-O2K_Fqw0', 
+    true
+),
+(   
+    'How does a blockchain work - Simply Explained', 'video', 'education', 
+    'https://www.youtube.com/embed/SSo_EIwHSd4', 
+    true
+),
+(   
+    'Cryptocurrency In 5 Minutes | Cryptocurrency Explained', 'video', 'education', 
+    'https://www.youtube.com/embed/1YyAzVmP9xQ', 
+    true
+),
+(   
+    'How Cryptocurrency ACTUALLY works', 'video', 'education', 
+    'https://www.youtube.com/embed/rYQgy8QDEBI', 
+    true
+),
+(   
+    'Cryptocurrency For Beginners Full Course', 'video', 'education', 
+    'https://www.youtube.com/embed/dgjZ2fHg4y4', 
     true
 );
